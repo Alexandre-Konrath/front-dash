5ed47d3c216678fb3e04c3231314e545d7e147d1
Alexandre-Konrath <delimakonrath@gmail.com>
2025-03-12 20:54:34 -0300

    feat: implementando conexão com o back

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IfoodService } from '../../services/ifood.service';
 
 @Component({
   selector: 'home-page',
@@ -10,6 +13,31 @@ import { CommonModule } from '@angular/common';
 })
 export class HomePage implements OnInit {
 
+  private ifoodService = inject(IfoodService)
+
+  userCode: string | null = null;
+  accessToken: string | null = null;
+
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+
+  // Obtém o userCode
+  getUserCode() {
+    this.ifoodService.getUserCode().subscribe((response) => {
+      this.userCode = response.userCode;
+      console.log('userCode obtido:', this.userCode);
+    });
+  }
+
+  // Obtém o accessToken com o userCode
+  getAccessToken() {
+    if (this.userCode) {
+      this.ifoodService.getToken(this.userCode).subscribe((response) => {
+        this.accessToken = response.accessToken;
+        console.log('Access Token:', this.accessToken);
+      });
+    }
+  }
+
 }
